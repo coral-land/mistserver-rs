a25b6e80417feaadd7e33893304c8e0c1ab52122
mehdi-ra <mehdirahimi.dev@gmail.com>
2026-06-10 09:01:30 +0330

    ci(tests.yaml): run tests on push

--- src/auth.rs
+++ src/auth.rs
@@ -78,18 +78,28 @@ impl MistAuthController {
     /// ### Example:
     ///
     /// ```rust
+    /// use mistserver_rs::{
+    ///  AuthResult, MistAuthController, Result, config::Config, http::client::build_http_client,
+    /// };
+    /// use std::{sync::Arc, time::Duration};
+    ///
     /// #[tokio::main]
-    /// async fn main() {
-    ///     let client = build_http_client();
-    ///     let config = Arc::new(config());
+    /// async fn main()-> Result<()> {
+    ///     let client = Arc::new(build_http_client(Duration::from_secs(10))?);
+    ///     let config = Arc::new(Config {
+    ///         mist_url: "http://localhost:8080".into(),
+    ///         auth: None,
+    ///     });
     ///
     ///     let mac = MistAuthController::new(client.clone(), config.clone());
-    ///     let auth_result: AuthResult = mac.authorize().await?;
-    ///
+    ///     let auth_result: AuthResult = mac.authorize().await.unwrap();
     ///     match auth_result {
     ///         AuthResult::NotRequired => {}
-    ///         AuthResult::Required(auth_response) => {}
+    ///         AuthResult::Required(_) => panic!("Expected NotRequired, got Required"),
     ///     }
+    ///
+    ///     assert!(matches!(auth_result, AuthResult::NotRequired));
+    ///     Ok(())
     /// }
     /// ```
     ///
