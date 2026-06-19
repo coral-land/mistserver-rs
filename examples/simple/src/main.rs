98870cd39440002ff6fb72f6ecf44e4d6667d46d
mehdi-ra <mehdirahimi.dev@gmail.com>
2026-06-20 02:12:13 +0330

    chore(examples): updated simple example

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,12 +1,15 @@
-use mistserver_rs::MistClientBuilder;
+use std::collections::HashMap;
+
+use mistserver_rs::{MistClientBuilder, StreamBuilder};
 use reqwest::Client;
 
 #[tokio::main]
 async fn main() {
     let client = Client::new();
+    let mut streams_hashmap = HashMap::new();
+
     let mut mist_client = MistClientBuilder::new("http://localhost:4242/api")
         .with_client(client)
-        // Better to load these from env :)
         .with_auth("admin", "password")
         .build();
 
@@ -16,6 +19,18 @@ async fn main() {
         }
         Ok(_) => {
             println!("Authorization successful");
+        }
+    }
+
+    let stream = StreamBuilder::new("push://").name("stream_one").build();
+    streams_hashmap.insert("stream_one".into(), stream);
+
+    match mist_client.streams().create(streams_hashmap).await {
+        Ok(r) => {
+            println!("insertion successful, {r:?}")
+        }
+        Err(e) => {
+            println!("insertion failed, e: {e:?}");
         }
     }
 }
